3e1bd0359998a813cdad78c54ea05658cd4c8ffa
Lucas Domin <dominguez_la@outlook.com>
2025-03-22 17:18:02 -0500

    character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import word_count
+from stats import word_count, string_character_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,7 +7,9 @@ def get_book_text(filepath):
 def main():
     text = get_book_text("books/frankenstein.txt")
     words = word_count(text)
+    character_count = string_character_count(text)
     print(f"{words} words found in the document")
+    print(f"{character_count} in the document")
     
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def word_count(text):
     words = text.split()
     return len(words)
+
+def string_character_count(text):
+    character_count = {}
+    text = text.lower()
+    for i in text:
+        if i in character_count:
+            character_count[i] += 1
+        else:
+            character_count[i] = 1
+    return character_count
