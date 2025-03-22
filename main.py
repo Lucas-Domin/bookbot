ad78af8a6832c45fc9ff1c133e4d3c0aed612446
Lucas Domin <dominguez_la@outlook.com>
2025-03-22 17:07:43 -0500

    refactoring

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+from stats import word_count
+
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
-
-def word_count(text):
-    words = text.split()
-    return len(words)
-
 
 def main():
     text = get_book_text("books/frankenstein.txt")
