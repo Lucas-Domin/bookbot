3ffa4ff30488a7019603f5013d3cc7f24299dfd9
Lucas Domin <dominguez_la@outlook.com>
2025-03-22 17:50:21 -0500

    character filtering, pretty output

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-from stats import word_count, string_character_count
+from stats import word_count, string_character_count, dictionary_to_list_sorted
+
+book_path = "books/frankenstein.txt"
 
 def get_book_text(filepath):
-    with open(filepath) as f:
+    with open(filepath, encoding='utf-8') as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    text = get_book_text(book_path)
     words = word_count(text)
     character_count = string_character_count(text)
-    print(f"{words} words found in the document")
-    print(f"{character_count} in the document")
-    
+    dict_list = dictionary_to_list_sorted(character_count)
+
+    print(f"""
+=========== BOOKBOT ============
+Analyzing book found at {book_path}...
+----------- Word Count ----------
+Found {words} total words
+--------- Character Count -------""")
+
+    for k, v in dict_list:
+        print(f"{k}: {v}")
 
 main()
--- stats.py
+++ stats.py
@@ -2,12 +2,25 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def clean(text):
+    cleaned = ""
+    for char in text:
+        if char.isalpha() and char.isascii(): # enforces standard english letters
+            cleaned += char
+    return cleaned
+
 def string_character_count(text):
+    text = clean(text.lower()) # lowercase for character count
     character_count = {}
-    text = text.lower()
     for i in text:
         if i in character_count:
             character_count[i] += 1
         else:
             character_count[i] = 1
     return character_count
+
+def dictionary_to_list_sorted(dictionary):
+    dict_list = list(dictionary.items()) # convert to list 
+    dict_list.sort(key=lambda x: x[1], reverse=True) # sort by count desc
+    return dict_list
+
