873c50be3439ea92e02a7eb04d469a5b7adb140c
Lucas Domin <dominguez_la@outlook.com>
2025-03-22 18:01:07 -0500

    takes book relative path as input

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from stats import word_count, string_character_count, dictionary_to_list_sorted
+import sys
 
-book_path = "books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    book_path = sys.argv[1]
 
 def get_book_text(filepath):
     with open(filepath, encoding='utf-8') as f:
@@ -11,15 +16,16 @@ def main():
     words = word_count(text)
     character_count = string_character_count(text)
     dict_list = dictionary_to_list_sorted(character_count)
+    return words, dict_list
 
-    print(f"""
+words, dict_list = main()
+
+print(f"""
 =========== BOOKBOT ============
 Analyzing book found at {book_path}...
 ----------- Word Count ----------
 Found {words} total words
 --------- Character Count -------""")
 
-    for k, v in dict_list:
-        print(f"{k}: {v}")
-
-main()
+for k, v in dict_list:
+    print(f"{k}: {v}")
